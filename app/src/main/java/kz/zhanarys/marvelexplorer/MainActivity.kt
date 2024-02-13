7273149251a288f863695afdd32526f39bf92937
Zhanarys Yeltayev <zhanarys.yeltayev@gmail.com>
2024-02-14 00:30:15 +0600

    Worked on App Theme for adapting to DayNight mode change

--- app/src/main/java/kz/zhanarys/marvelexplorer/MainActivity.kt
+++ app/src/main/java/kz/zhanarys/marvelexplorer/MainActivity.kt
@@ -1,28 +1,22 @@
 package kz.zhanarys.marvelexplorer
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.createGraph
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.fragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kz.zhanarys.data.repositories.local.CharactersLocalRepository
-import kz.zhanarys.domain.ViewState
 import kz.zhanarys.marvelexplorer.databinding.ActivityMainBinding
-import kz.zhanarys.marvelexplorer.charactersList.CharactersListFragment
-import kz.zhanarys.marvelexplorer.favoritesListPage.FavoritesListFragment
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
     @Inject lateinit var charactersLocalRepository: CharactersLocalRepository
 
     @Inject lateinit var viewModelFactory: SharedViewModelFactory
@@ -33,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE)
         setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainFragmentContainer) as NavHostFragment
